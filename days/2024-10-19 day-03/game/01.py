2e532866423857d000540a62a3a0e57c5d8c94e4
permCoding <ttxiom@gmail.com>
2024-10-19 14:38:17 +0500

    up

--- days/2024-10-19 day-03/game/01.py
+++ days/2024-10-19 day-03/game/01.py
@@ -2,14 +2,17 @@
 from random import randint as rnd
 
 comp = rnd(1, 1_000)
-money  =1_000  # $
+money = 1_000  # $
 while True:
-    n = input('введите число - ')
-    # if break
-    # if L
-    # if R
+    n = int(input('введите число - '))
+    if comp == n: break
+    if comp < n:
+        print("- меньше")
+    else:
+        print("- больше")
+    money -= 100
 
-print()
+print(f'осталось - {money} $')
 
 """
 500
