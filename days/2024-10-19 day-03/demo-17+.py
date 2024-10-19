c955f3ee50d65453cc1fd4e4719fcd665efe3cdb
permCoding <ttxiom@gmail.com>
2024-10-19 14:13:11 +0500

    up

--- days/2024-10-19 day-03/demo-17+.py
+++ days/2024-10-19 day-03/demo-17+.py
@@ -2,12 +2,20 @@ nums = [int(e) for e in open('./demo_2025_17.txt')]
 
 mn = min(nums)
 
-k = 0
+k, max_p = 0, 0
 for i in range(0, len(nums)-1):
     if nums[i]%16 == mn or nums[i+1]%16 == mn:
         k += 1
+        if nums[i]+nums[i+1] > max_p:
+            max_p = nums[i]+nums[i+1]
 
-print(k)
+print(k, max_p)
+
+
+
+
+
+
 
 # len(nums)-2  len(nums)-1
 # 1 2 3 4 5
---
+++ days/2024-10-19 day-03/game/01.py
@@ -0,0 +1,25 @@
+# алгоритм бинарного поиска
+from random import randint as rnd
+
+comp = rnd(1, 1_000)
+money  =1_000  # $
+while True:
+    n = input('введите число - ')
+    # if break
+    # if L
+    # if R
+
+print()
+
+"""
+500
+250
+125
+62
+32
+16
+8
+4
+2
+1
+"""
