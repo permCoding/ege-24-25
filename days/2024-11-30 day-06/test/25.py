4ab12aef6fb30ebc4b5d641c3b6fb7d558de94d0
Андрей Беляков <ttxiom@gmail.com>
2024-11-29 20:49:11 +0500

    up

--- days/2024-11-30 day-06/test/25.py
+++ days/2024-11-30 day-06/test/25.py
@@ -1,6 +1,29 @@
-for n in range(326782, 965324):
+def get_d(n):
+    t = []
+    for d in range(2, int(n**.5)+1):
+        if n%d==0: t += [d, n//d]
+    return list(set(t))
+    
+def p(n):
+    for d in range(2, int(n**.5)+1):
+        if n%d==0: return False
+    return True
+
+for n in range(326782, 965325):
     t = get_d(n)
-    if t[0]*t[1]*t[2]==n:
-        if p(t[0]) and p[t[1]] and p[t[2]]:
-            if max(t)-min(t) <= 12:
-                print(n)
+    w = [d for d in t if p(d)]
+    if len(w) == 3:
+        if w[0]*w[1]*w[2]==n:
+            diff = max(w)-min(w)
+            if diff <= 12: print(n, diff)
+"""
+347261 6
+375803 12
+386389 12
+409457 8
+430189 12
+465547 12
+478661 10
+583573 10
+871933 12
+"""
