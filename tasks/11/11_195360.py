cde8febe8a70a02ac018bd047882641a15878e55
Андрей Беляков <ttxiom@gmail.com>
2025-04-02 16:11:18 +0500

    up

---
+++ tasks/11/11_195360.py
@@ -0,0 +1,7 @@
+from math import ceil
+
+a = 33 + 33 + 10 + 50*5  # 326
+bt = 9
+
+n = 1016
+print(ceil(n * bt / 8))  # 1143
---
+++ tasks/11/11_19561.py
@@ -0,0 +1,7 @@
+from math import ceil
+
+n = 399
+b = ceil(n * 10 / 8)
+
+print(b * 725)
+print(353 * 1024)
