382b308f9f339dd74dc0f7fc860718a0de6f3ec4
Андрей Беляков <ttxiom@gmail.com>
2025-05-24 01:40:38 +0500

    up

---
+++ tasks/24/24_20909-.py
@@ -0,0 +1,16 @@
+s = open('./24_20909.txt').readline().replace('AB', '#')
+# s = 'AABAAABAB'.replace('AB', '#')
+g = 100
+t = []
+l, k = 0, 0
+for r in range(0, len(s)):
+    if s[r] == '#': k += 1
+    if k == g:
+        ln = r-l+1
+        t += [ln]
+    while k > g:
+        if s[l] == '#': k -= 1
+        l += 1
+
+m = max(t)
+print(m+g)  # 748 - а должно быть => 750
